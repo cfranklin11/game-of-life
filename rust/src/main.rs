6899546e3a09a3bbc8d63e585677df1b7c53c6e8
Craig Franklin <craigjfranklin@gmail.com>
2018-05-20 16:49:54 +1000

    Random init

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,17 @@
+extern crate rand;
+
 fn main() {
-    println!("Hello, world!");
+    const GRID_SIZE: u8 = 5;
+    let mut grid = Vec::new();
+
+    for _n in 0..GRID_SIZE {
+        let mut row: Vec<bool> = Vec::new();
+
+        for _m in 0..GRID_SIZE {
+            row.push(rand::random());
+        }
+
+        println!("{:?}", row);
+        grid.push(row);
+    }
 }
